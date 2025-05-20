50046952d3814707d5d9557cd9f67bba2992041e
vineet-repos <75121372+vineet-repos@users.noreply.github.com>
2025-05-20 12:15:49 +0530

    Update train.py
    Added slit_data function definition and autolog

--- src/model/train.py
+++ src/model/train.py
@@ -5,16 +5,15 @@ import glob
 import os
 
 import pandas as pd
-
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 
 
 # define functions
-def split_data(df):
-    null
+    
 def main(args):
     # TO DO: enable autologging
-
+    mlflow.autolog()
 
     # read data
     df = get_csvs_df(args.training_data)
@@ -36,7 +35,10 @@ def get_csvs_df(path):
 
 
 # TO DO: add function to split data
-
+def split_data(df):
+    X, y = df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','Age']].values, df['Diabetic'].values
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
+    return  X_train, X_test, y_train, y_test
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # train model
